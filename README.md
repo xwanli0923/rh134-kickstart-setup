745c763031ba46265fcdbca100336aa8b881fab5
xwanli0923 <xingwanli@outlook.com>
2021-12-30 15:46:08 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # rh134-kickstart-setup
-For Red Hat Enterprise Linux 8 RH134 (System Management II) Chapter 12
+
+#### 介绍
+For Red Hat Enterprise Linux 8  RH134 (Red Hat System Administration II) Chapter 12
+
+#### 软件架构
+X86_64
+
+
+#### 课程版本
+RH134-RHEL8
+#### 安装教程
+
+1.  将课程切换到`RH134`或者`RH199`
+2.  重置 `servera` 节点
+3.  将 `lab-pxekickstart.tar.gz` 复制到 `servera` 并使用脚本 `deploy-pxe-server.sh` 部署服务
+4.  在 `foundation0` 使用 `root` 账户运行 `virsh-install-serverc.sh` 脚本
+
+#### 使用说明
+
+1.  重置 `servera`
+```bash
+$ rht-vmctl reset servera
+```
+2.  将 `lab-pxekickstart.tar.gz` 复制到`servera`
+```bash
+cd rh134-kickstart-setup/
+scp lab-pxekickstart.tar.gz root@servera:~
+```
+3. 在 `servera` 部署服务器
+```bash
+tar -xf lab-pxekickstart.tar.gz
+cd lab-pxekickstart/
+./deploy-pxe-server.sh
+```
+4. 回退到 `foundation`,创建新的虚拟机`serverc`
+ ```bash
+ ./virt-install-serverc.sh
+ ```
+ 在打开的 `console` 中，选择**Install RHEL8.2 by Kickstart*
+
+#### 作者
+**邢万里 | Augus_Hsing**
+**Email**: <a href="mailto:wanly0923@qq.com">wanly0923@qq.com</a>
---
+++ virt-install-serverc.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# add dns records
+echo -e "Setting bastion dns...\n"
+sleep 1
+rsync -P hosts root@bastion:/etc/hosts >/dev/null
+ssh root@bastion "systemctl restart dnsmasq.service"  >/dev/null
+# add a new virtual machine serverc
+echo -e "Add a new virtual machine serverc...\n"
+sleep 1
+if [ $UID -ne 0 ];then
+	echo ""Error: This command has to be run under the root user.
+else
+# Check whether serverc exists, and delete it if it exists
+virsh list --all | grep serverc &>/dev/null
+if [ $? -eq 0 ];then
+	echo "Delete the existing serverc..."
+	virsh destroy --domain serverc  2>/dev/null
+	virsh undefine --domain serverc
+ 	rm -f /var/lib/libvirt/images/servere-vda.qcow2
+	ps -Ao comm | grep virt-viewer
+	if [ $? -eq 0 ];then
+	kill -9 $(ps -Ao pid,comm | grep 'virt-viewer' | awk '{print $1}')
+	fi
+fi
+virt-install \
+	--name serverc \
+	--memory 2048 \
+	--vcpus 2 \
+	--disk /var/lib/libvirt/images/servere-vda.qcow2,size=10,format=qcow2,target.bus=virtio \
+	--network network=privbr0,mac.address=52:54:00:00:fa:0c,model=virtio \
+	--graphics type=spice,listen=127.0.0.1 \
+	--noautoconsole \
+	--os-type=linux \
+	--os-variant rhel8.2 \
+	--machine q35 \
+	--boot hd,network,cdrom >/dev/null
+	#--boot uefi \
+	#--extra-args "ks=http://servera.lab.example.com/ks/install.ks" \
+	#--cdrom=/content/rhel8.2/x86_64/isos/rhel-8.2-x86_64-dvd.iso 
+#--machine KVM \
+	#--install kernel_args="console=tty0 console=ttyS0,115200n8" \
+	#--graphics type=spice,port=5908,tlsPort=5908,listen=127.0.0.1 \
+virt-viewer serverc &
+#echo -e "Plese use user kiosk and below command view the serverc\n"
+#echo "virt-viewer -c qemu:///system serverc &> /dev/null &"
+fi
